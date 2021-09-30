3e6db291f7031bedd20c8f359ed75affbe0b20fd
unknown <jailsonp437@gmail.com>
2021-09-30 17:07:43 -0400

    Tabela de IMC adicionada indicando qual sua situação atual

--- script.js
+++ script.js
@@ -9,9 +9,24 @@ function CalcularIMC() {
     let peso = Number(CampoPeso.value)
     let altura = Number(CampoAltura.value)
 
-    let IMC = peso / (altura * altura)
+    let IMC = (peso / (altura * altura)).toFixed(2)
 
-    res.innerHTML = IMC.toFixed(2)
+    //res.innerHTML = IMC.toFixed(2)
+    if (IMC == 'NaN') {
+        res.innerHTML = 'Digite um valor válido'
+    } if (IMC < 18.5){
+        res.innerHTML = `IMC: ${IMC}, abaixo do peso`
+    } if (IMC >= 18.5 && IMC <= 24.9) {
+        res.innerHTML = `IMC: ${IMC}, peso normal`
+    }  if (IMC >= 25 && IMC <= 29.9) {
+        res.innerHTML = `IMC: ${IMC}, sobrepeso`
+    }  if (IMC >= 30 && IMC <= 34.9) {
+        res.innerHTML = `IMC: ${IMC}, obesidade grau I`
+    }  if (IMC >= 35 && IMC <= 39.9) {
+        res.innerHTML = `IMC: ${IMC}, obesidade grau II`
+    }  if (IMC >= 40) {
+        res.innerHTML = `IMC: ${IMC}, Obesidade grau III ou mórbida`
+    }
 }
 
 function reset() {
@@ -20,5 +35,3 @@ function reset() {
     CampoAltura.value = ''
     CampoPeso.value = ''
 }
-
-console.log('ola')
